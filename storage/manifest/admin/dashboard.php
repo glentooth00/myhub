6ec6301568943430fa72161f569d991fa0d4a493
Xavier <xavier.tnc@gmail.com>
2024-07-09 18:18:29 +0200

    Remove .env-local and junk files.

--- storage/manifest/admin/dashboard.php
+++
@@ -1,6 +0,0 @@
-<?php return array (
-  'C:\\laragon\\www\\myhub\\pages\\admin\\dashboard\\dashboard.html' => 1714953008,
-  'C:\\laragon\\www\\myhub\\pages\\_templates\\default-theme\\admin/head.html' => 1717220468,
-  'C:\\laragon\\www\\myhub\\pages\\admin\\dashboard\\_partials/show.html' => 1719196544,
-  'C:\\laragon\\www\\myhub\\pages\\_templates\\default-theme\\admin/tail.html' => 1714953000,
-); ?>
--- storage/manifest/admin/referrers-list-partial.php
+++
@@ -1,5 +0,0 @@
-<?php return array (
-  'C:\\laragon\\www\\myhub\\pages\\admin\\referrers\\_partials\\list.html' => 1715195724,
-  'C:\\laragon\\www\\myhub\\pages\\admin\\referrers\\_partials/list.css' => 1715195724,
-  'C:\\laragon\\www\\myhub\\pages\\admin\\referrers\\_partials/list.js' => 1715195724,
-); ?>
